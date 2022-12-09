a6bf4273fa30c96957613eb82295bf34d5f6abd5
petetnt <pete.a.nykanen@gmail.com>
2022-12-09 15:15:37 +0200

    Add functionality for form validation
    Signed-off-by: petetnt <pete.a.nykanen@gmail.com>

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,16 +1,29 @@
 import type { ActionArgs } from "@remix-run/node";
-import { redirect } from "@remix-run/node";
+import { redirect, json } from "@remix-run/node";
+import { useActionData, Form, useTransition} from "@remix-run/react";
 
 export const action = async ({ request }: ActionArgs) => {
   const formData = await request.formData();
+  const email = formData.get("email");
+  const password = formData.get("password");
 
-  if (formData) {
-    return {};
+  if (email !== "test@vincit.com" || password !== "testpassword") {
+    return json({
+      errors: "Email or password was wrong",
+      values: {
+        email: email?.toString(),
+        password: password?.toString()
+      }
+    })
   }
+  
   return redirect(`/dashboard`);
 };
 
 export default function Index() {
+  const transition = useTransition();
+  const actionData = useActionData<typeof action>();
+
   return (
     <>
       <div className="text-center bg-gray-50 text-gray-800 py-20 px-6">
@@ -32,7 +45,7 @@ export default function Index() {
               />
             </div>
             <div className="xl:ml-20 xl:w-5/12 lg:w-5/12 md:w-8/12 mb-12 md:mb-0">
-              <form method="post" action="/">
+              <Form method="post" action="/?index">
                 <div className="flex flex-row items-center justify-center lg:justify-start">
                   <p className="text-lg mb-0 mr-4">Sign in</p>
                 </div>
@@ -42,25 +55,34 @@ export default function Index() {
                 />
                 <div className="mb-6">
                   <input
+                    disabled={transition.state === "submitting"}
+                    name="email"
                     type="text"
                     className="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
-                    id="exampleFormControlInput2"
+                    id="exampleFormControlInput1"
                     placeholder="Email address"
+                    defaultValue={actionData?.values.email}
                   />
                 </div>
 
                 <div className="mb-6">
                   <input
+                    disabled={transition.state === "submitting"}
+                    name="password"
                     type="password"
                     className="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
                     id="exampleFormControlInput2"
                     placeholder="Password"
+                    defaultValue={
+                      actionData?.values.password
+                    }
                   />
                 </div>
 
                 <div className="flex justify-between items-center mb-6">
                   <div className="form-group form-check">
                     <input
+                      disabled={transition.state === "submitting"}
                       type="checkbox"
                       className="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
                       id="exampleCheck2"
@@ -72,9 +94,13 @@ export default function Index() {
                   <a href="/forgot-password" className="text-gray-800">Forgot password?</a>
                 </div>
 
+
+                {actionData?.errors &&
+                  <div className="bg-red-50 mb-5 p-5">{actionData?.errors}</div>
+                }
+
                 <div className="text-center lg:text-left">
                   <button
-                    type="button"
                     className="inline-block px-7 py-3 bg-blue-600 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
                   >
                     Login
@@ -88,7 +114,7 @@ export default function Index() {
                     >
                   </p>
                 </div>
-              </form>
+              </Form>
             </div>
           </div>
         </div>
